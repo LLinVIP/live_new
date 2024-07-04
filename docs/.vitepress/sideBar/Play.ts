03e25b7fbcf5dd2936a2651e46d376aba197eef3
Yolo3 <1282001814@qq.com>
2024-07-04 22:44:43 +0800

    feat(docs): add new restaurants to the Eat category and update markdown formatting
    - Add '垦牛堡·手作牛肉汉堡（儿童医院店）' to the TakeAway section.
    - Create new sections for '泉山区' and '云龙区' under the OutSchool subsection,
      adding several restaurants in each area.
    - Update the formatting in 'ChaTraMue手标泰式茶(徐州文庙店)' markdown file to
      improve readability and correct typos.
    - Increase the version number in package.json to reflect the changes.

--- docs/.vitepress/sideBar/Play.ts
+++ docs/.vitepress/sideBar/Play.ts
@@ -13,7 +13,7 @@ export default function PlaySideBar() {
                 {
                   text: '🍄‍🟫 外卖美食',
                   items: [
-                    { text: '校外交通', link: '/Life/Traffic/OutSchool' },
+                    { text: '垦牛堡·手作牛肉汉堡（儿童医院店）', link: '/Play/Eat/TakeAway/垦牛堡·手作牛肉汉堡（儿童医院店）' },
                    ],
                     collapsed: false
               },   
@@ -28,9 +28,35 @@ export default function PlaySideBar() {
                           collapsed: false
                     },
                     {
-                        text: '学校附近',
+                      text: '泉山区',
+                      items: [
+                        { text: '多福临砂锅居', link: '/Play/Eat/OutSchool/QuanShan/多福临砂锅居' },
+                        { text: '巨鲸pizza', link: '/Play/Eat/OutSchool/QuanShan/巨鲸pizza' },
+                        { text: '南京大排档', link: '/Play/Eat/OutSchool/QuanShan/南京大排档' },
+                        { text: '武吉制面日式料理', link: '/Play/Eat/OutSchool/QuanShan/武吉制面日式料理' },
+                        { text: '辛香汇', link: '/Play/Eat/OutSchool/QuanShan/辛香汇' },
+                        { text: '周老六安庆面馆', link: '/Play/Eat/OutSchool/QuanShan/周老六安庆面馆' },
+                        { text: 'L&C Bar', link: '/Play/Eat/OutSchool/QuanShan/L&C Bar' }
+                       ],
+                        collapsed: false
+                  },
+                  {
+                    text: '云龙区',
+                    items: [
+                      { text: '鹿巷甜品', link: '/Play/Eat/OutSchool/YunLong/鹿巷甜品' },
+                      { text: '马市街饣它汤', link: '/Play/Eat/OutSchool/YunLong/马市街饣它汤' },
+                      { text: '味旬甜品', link: '/Play/Eat/OutSchool/YunLong/味旬甜品' },
+                      { text: '蟹逅龙虾', link: '/Play/Eat/OutSchool/YunLong/蟹逅龙虾' },
+                      { text: 'Temp度C', link: '/Play/Eat/OutSchool/YunLong/Temp度C' },
+                     ],
+                      collapsed: false
+                },
+                    {
+                        text: '小南门',
                         items: [
                           { text: '凉菜', link: '/Play/Eat/OutSchool/Vicinity/凉菜' },
+                          { text: '明记肠粉', link: '/Play/Eat/OutSchool/Vicinity/明记肠粉' },
+                          { text: '夜归人菜馆', link: '/Play/Eat/OutSchool/Vicinity/夜归人菜馆' }
                          ],
                           collapsed: false
                     }
---
+++ docs/Play/Eat/OutSchool/QuanShan/L&C Bar.md
@@ -0,0 +1,35 @@
+---
+author: Clo0oOoud
+---
+
+# L&C Bar
+
+#111
+
+## :mag:位置
+
+文昌老校区三胞广场那片，泉山区人民政府正对面
+
+![image-20240412135039355](https://s2.loli.net/2024/04/12/qiKw26mWgJQdyCI.png)
+
+## 🌰 食评
+
+清吧，店面不太大，但氛围很好，酒吧界里的深夜食堂的感觉，是周末避世的好去处。一个人去喝或者几个好友一起去喝都可以，没有包间，可以选择坐吧台或者靠里的卡座。有衣架可以挂外套。老板和调酒师都很好说话，人不多的时候会问你喜欢什么样的酒，按你喜欢的推荐调制。喜欢酸甜苦辣和 dry 的都能找到喜欢的，酒单挺丰富。
+
+德式香肠披萨味道比较一般，就不在此推荐了。
+
+## :tipping_hand_man:Tips
+
+美团和大众点评都有团购券，折下来 45 块钱一杯鸡尾酒，点了多少杯就团多少张券，可以叠加。
+
+![image-20240412135123959](https://s2.loli.net/2024/04/12/RYsqHwCyxfZ6tla.png)
+
+![image-20240412135209311](https://s2.loli.net/2024/04/12/DXv4zfNr3kKBUqh.png)
+
+![image-20240412135220494](https://s2.loli.net/2024/04/12/PEcUpBSXDYJglVM.png)
+
+![image-20240412135318541](https://s2.loli.net/2024/04/12/tceCm4L5uvJ2r7x.png)
+
+![image-20240412135330888](https://s2.loli.net/2024/04/12/9YqSVOkZEG81Uty.png)
+
+![image-20240412135344267](https://s2.loli.net/2024/04/12/erQBCSvdEVyh7xK.png)
