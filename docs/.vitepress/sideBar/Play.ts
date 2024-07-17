4c0f049704e036aba6c644ef03b99ce25e39e580
Yolo3 <1282001814@qq.com>
2024-07-18 00:28:21 +0800

    feat(docs): add new restaurants to the Play sidebar and update network documentation
    - Add several new restaurant links to the Play sidebar under different categories.
    - Update the documentation for the integrated network to reflect changes and improve
      clarity.
    - Revise the content for a more accurate and up-to-date representation of the
      university's dining options and network services.

--- docs/.vitepress/sideBar/Play.ts
+++ docs/.vitepress/sideBar/Play.ts
@@ -24,6 +24,10 @@ export default function PlaySideBar() {
                         text: '鼓楼区',
                         items: [
                           { text: 'ChaTraMue手标泰式茶', link: '/Play/Eat/OutSchool/GuLou/ChaTraMue手标泰式茶' },
+                          { text: '郭际辣饼铺', link: '/Play/Eat/OutSchool/GuLou/郭际辣饼铺' },
+                          { text: '九里羊汤馆', link: '/Play/Eat/OutSchool/GuLou/九里羊汤馆' },
+                          { text: '三中高皇羊肉馆', link: '/Play/Eat/OutSchool/GuLou/三中高皇羊肉馆' },
+                          { text: '维卡食舍', link: '/Play/Eat/OutSchool/GuLou/维卡食舍' }
                          ],
                           collapsed: true
                     },
@@ -36,7 +40,15 @@ export default function PlaySideBar() {
                         { text: '武吉制面日式料理', link: '/Play/Eat/OutSchool/QuanShan/武吉制面日式料理' },
                         { text: '辛香汇', link: '/Play/Eat/OutSchool/QuanShan/辛香汇' },
                         { text: '周老六安庆面馆', link: '/Play/Eat/OutSchool/QuanShan/周老六安庆面馆' },
-                        { text: 'L&C Bar', link: '/Play/Eat/OutSchool/QuanShan/L&C Bar' }
+                        { text: 'L&C Bar', link: '/Play/Eat/OutSchool/QuanShan/L&C Bar' },
+                        { text: '橙鱼岛三文鱼', link: '/Play/Eat/OutSchool/QuanShan/橙鱼岛三文鱼' },
+                        { text: '出炉食', link: '/Play/Eat/OutSchool/QuanShan/出炉食' },
+                        { text: '黑牛无双', link: '/Play/Eat/OutSchool/QuanShan/黑牛无双' },
+                        { text: '卢氏跑马汤', link: '/Play/Eat/OutSchool/QuanShan/卢氏跑马汤' },
+                        { text: '青辰法式甜品', link: '/Play/Eat/OutSchool/QuanShan/青辰法式甜品' },
+                        { text: '三顺甜品店', link: '/Play/Eat/OutSchool/QuanShan/三顺甜品店' },
+                        { text: '四湖鱼村', link: '/Play/Eat/OutSchool/QuanShan/四湖鱼村' },
+                        { text: '一家人人生一串', link: '/Play/Eat/OutSchool/QuanShan/一家人人生一串' },
                        ],
                         collapsed: true
                   },
@@ -48,6 +60,9 @@ export default function PlaySideBar() {
                       { text: '味旬甜品', link: '/Play/Eat/OutSchool/YunLong/味旬甜品' },
                       { text: '蟹逅龙虾', link: '/Play/Eat/OutSchool/YunLong/蟹逅龙虾' },
                       { text: 'Temp度C', link: '/Play/Eat/OutSchool/YunLong/Temp度C' },
+                      { text: '莞记', link: '/Play/Eat/OutSchool/YunLong/莞记' },
+                      { text: '嘉简诚厨', link: '/Play/Eat/OutSchool/YunLong/嘉简诚厨' },
+                      { text: '许老八包子铺', link: '/Play/Eat/OutSchool/YunLong/许老八包子铺' },
                      ],
                       collapsed: true
                 },
@@ -56,7 +71,11 @@ export default function PlaySideBar() {
                         items: [
                           { text: '凉菜', link: '/Play/Eat/OutSchool/Vicinity/凉菜' },
                           { text: '明记肠粉', link: '/Play/Eat/OutSchool/Vicinity/明记肠粉' },
-                          { text: '夜归人菜馆', link: '/Play/Eat/OutSchool/Vicinity/夜归人菜馆' }
+                          { text: '夜归人菜馆', link: '/Play/Eat/OutSchool/Vicinity/夜归人菜馆' },
+                          { text: '红枣坊', link: '/Play/Eat/OutSchool/Vicinity/红枣坊' },
+                          { text: '鸡柳大人', link: '/Play/Eat/OutSchool/Vicinity/鸡柳大人' },
+                          { text: '红红火火鸡柳烧饼', link: '/Play/Eat/OutSchool/Vicinity/红红火火鸡柳烧饼' },
+                          { text: '果遇鲜果铺', link: '/Play/Eat/OutSchool/Vicinity/果遇鲜果铺' },
                          ],
                         collapsed: true
                     }
