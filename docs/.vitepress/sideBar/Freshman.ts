528209a11b5fe3053768fe1c68f54eabdacdd617
Yolo3 <1282001814@qq.com>
2024-08-01 00:38:01 +0800

    添加'各类群'链接到Freshman侧边栏菜单
    在Freshman侧边栏的配置中，新增了一个链接项'各类群'，以便用户能够更容易地访问到相关群信息页面。这一改动旨在改善用户导航体验，帮助新生更好地了解和参与各类群活动。

--- docs/.vitepress/sideBar/Freshman.ts
+++ docs/.vitepress/sideBar/Freshman.ts
@@ -1,6 +1,7 @@
 export default function FreshmanSideBar() {
   return [
     
-        { text: '浅谈入学前的准备', link: '/Freshman/浅谈入学前的准备' }
+        { text: '浅谈入学前的准备', link: '/Freshman/浅谈入学前的准备' },
+        { text: '各类群', link: '/Freshman/各类群' },
     ]
 }
