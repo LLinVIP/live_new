bb2940bc97e9a4af161e4e1faf326ad726f4ce27
Yolo3 <1282001814@qq.com>
2024-08-05 15:02:51 +0800

    feat(docs): 添加矿大地图快速上手指南并更新依赖
    - 在新生文档中添加“矿大地图快速上手”指南。
    - 更新至vitepress 1.3.1以修复已知问题。
    - 更新项目依赖以保持兼容性和稳定性。

--- docs/.vitepress/sideBar/Freshman.ts
+++ docs/.vitepress/sideBar/Freshman.ts
@@ -3,5 +3,6 @@ export default function FreshmanSideBar() {
     
         { text: '浅谈入学前的准备', link: '/Freshman/浅谈入学前的准备' },
         { text: '各类群', link: '/Freshman/各类群' },
+        { text: '矿大地图快速上手', link: '/Freshman/矿大地图快速上手' },
     ]
 }
