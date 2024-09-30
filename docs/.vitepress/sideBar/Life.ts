7cc512249662d581d06eb65db1b589feebe0d40c
Yolo3 <yskysk1177@gmail.com>
2024-10-01 00:33:02 +0800

    文档更新和依赖升级
    - 在健康相关文档中添加了就诊指南
    - 升级了多个依赖，包括node、postcss、unocss、unplugin-icons、vite和vue

--- docs/.vitepress/sideBar/Life.ts
+++ docs/.vitepress/sideBar/Life.ts
@@ -28,6 +28,7 @@ export default function LifeSideBar() {
       text: '💊 健康在矿大',
       items: [
         { text: '疫苗', link: '/Life/Health/Vaccine' },
+        { text: '就诊', link: '/Life/Health/Visit' },
         { text: '医疗保险', link: '/Life/Health/Medicare' },
        ],
         collapsed: false
