bc39203f2a7557b4d556e59f1dd6d565275c9bb0
Yolo3 <yskysk1177@gmail.com>
2024-11-13 13:12:20 +0800

    docs: 更新物流、银行和课程相关文档
    - 移除 GlobalComponents 中的未使用图标声明
    - 更新 Logistics.md 中的邮政取件说明和收发室信息
    - 添加 BankCard.md 中的银行卡作用介绍，更新激活流程和管理部门信息
    - 在 PE.md 中增加体育课缓考说明
    - 更新 PublicElectiveCourse.md 中的选课时间和相关规定

--- docs/.vitepress/components.d.ts
+++ docs/.vitepress/components.d.ts
@@ -7,14 +7,9 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
-    'Bi:fileEarmarkWordFill': typeof import('~icons/bi/file-earmark-word-fill')['default']
     Copyright: typeof import('./theme/components/Copyright.vue')['default']
-    'EosIcons:modifiedDate': typeof import('~icons/eos-icons/modified-date')['default']
     HomeContributors: typeof import('./theme/components/HomeContributors.vue')['default']
-    'Octicon:feedPerson16': typeof import('~icons/octicon/feed-person16')['default']
-    'Ooui:clock': typeof import('~icons/ooui/clock')['default']
     PageInfo: typeof import('./theme/components/PageInfo.vue')['default']
     Progress: typeof import('./theme/components/Progress.vue')['default']
-    'RadixIcons:update': typeof import('~icons/radix-icons/update')['default']
   }
 }
